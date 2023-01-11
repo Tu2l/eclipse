35c04dc00096af07f2865d415114e1879831a7de
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-11 17:12:20 +0530

    Jenkin build test

--- GitDemoProject/src/main/java/com/cg/App.java
+++ GitDemoProject/src/main/java/com/cg/App.java
@@ -4,10 +4,8 @@ package com.cg;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello jenkins!");
+	}
 }
